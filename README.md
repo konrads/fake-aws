759c553f97bce71133330697fb06a5d0dbd80301
Konrad Sosnowski <konrad.sosnowski@ubnt.com>
2016-01-03 20:01:54 +0000

    adding data population story

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Local implementations of AWS services:
 * ElasticCache (redis)
 
 Creates necessary docker images, containers, populates data.
+
+To run:
+```bash
+# refresh docker containers
+> bin/fake-aws refresh all
+
+# populate data for docker-machine's host 192.168.99.100
+> data_dir=example/aws-data endpoint_host=192.168.99.100 bin/aws-populate refresh all
+```
---
+++ bin/commons.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+green="\033[32m"
+red="\033[31m"
+bold="\033[1m"
+reset="\033[0m"
+
+log_green() {
+  echo -e "${green}$@${reset}"
+}
+
+log_bold() {
+  echo -e "${bold}$@${reset}"
+}
+
+log_red() {
+  echo -e "${red}$@${reset}"
+}
+
+get_docker_ip() {
+  docker inspect -f '{{ .NetworkSettings.IPAddress }}' $1
+}
+
+assert_eq() {
+  if [ "$2" != "$3" ]; then
+    echo -e "  ${red}$1: $2 != $3${reset}"
+    exit -1
+  fi
+}
+
+ensure_tools() {
+  set +e
+  for tool_check in "${@}"
+  do
+    # log_bold "  ..testing tool $tool_check"
+    $tool_check > /dev/null
+    assert_eq "$tool_check exit code" $? 0
+  done
+  set -e
+}
